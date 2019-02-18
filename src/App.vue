b351d1969bda3d19bbd274fb71e38e1f03d25027
cigalecigales <cigalecigales@gmail.com>
2019-02-18 22:52:33 +0900

    create sample view

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="logo_area">
+      <img alt="Vue logo" src="./assets/logo.png" style="width: 100px;">
+    </div>
+    <div class="input_area">
+      <input type="text" placeholder="Find your favorite conference ..." class="input_text">
+    </div>
+    <div class="card_area">
+      <Card v-for="d in data" :data="d" :key="d.id" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
+import data from './data/items.json'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Card
+  },
+  data() {
+    return {
+      data: data
+    }
   }
 }
 </script>
 
 <style>
-#app {
+* {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  font-size: 16px;
+  box-sizing: border-box;
+  outline: none;
+}
+
+html, body {
+  background: #fdfdfd;
+}
+
+.logo_area {
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.input_area {
+  width: 95%;
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+.input_text {
+  width: 100%;
+  padding: 5px 8px;
+  border-radius: 3px;
+  border: 1px solid #ffbb87;
+}
+
+.input_text:focus {
+  border: 1px solid #f57868;
+}
+
+.card_area {
+  margin: 0 auto;
+  margin-bottom: 50px;
+  padding: 5px;
+  width: 95%;
+  column-count: 5;
+  column-gap: 10px;
+  margin-top: 30px;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <div class="name">
+      <a :href="data.url" target="_blank">
+        {{ data.name }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    data: Object
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  background: #ffffff;
+  border: 1px solid #dddddd;
+  border-radius: 3px;
+  padding: 10px;
+}
+</style>
